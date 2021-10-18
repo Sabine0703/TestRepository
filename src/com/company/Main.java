45211f8e82bdd51f567143b209ec2ac0a4ec9bd8
Sabine0703 <sabine.sk0703@gmail.com>
2021-10-18 21:00:23 +0300

    Added new entry from dev branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         String str = "Sabīne Strauta";
         System.out.println(str);
+        System.out.println("Another entry");
+        System.out.println("This is dev branch");
     }
 }
